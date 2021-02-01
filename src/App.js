d1e2f3e74a4f13a6c51eca7976d2efda7dd7c388
Peter-Hai-0 <2298288264@qq.com>
2021-02-01 16:10:26 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/index.css';
+import Axios from 'axios';
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import Tuijian from './components/tuijian'
+import Head from './components/head'
+import Tail from './components/tail'
+import Keji from './components/keji'
+import Redian from './components/redian'
+import Yule from './components/yule'
+import Youxi from './components/youxi'
+import Zhibo from './components/zhibo'
+import Shuma from './components/shuma'
+import Caijing from './components/caijing'
+import Login from './components/login'
+import Me from './components/me'
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: ''
+        }
+    }
+
+    searchMsgK = (e) => {
+        if (e.keyCode == 13) {
+            alert(this.inputValue.value)
+        }
+
+    }
+    searchMsgB = () => {
+        this.searchMsgK();
+    }
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    < Head/>
+                    <header className="title">今日头条
+                        <div className="search-wrapper" style={{float: 'right'}}>
+                            <input ref={(input) => this.inputValue = input} onKeyUp={this.searchMsgK}
+                                   placeholder="搜索站内资讯、视频或用户"/>
+                            <button type="button" onChange={this.searchMsgB}>搜索</button>
+
+                        </div>
+                    </header>
+                    <br/>
+                    <div className="tab">
+                        <Link to="/">推荐</Link>
+                        <Link to="/redian">热点</Link>
+                        <Link to="/keji">科技</Link>
+                        <Link to="/zhibo">直播</Link>
+                        <Link to="/youxi">游戏</Link>
+                        <Link to="/yule">娱乐</Link>
+                        <Link to="/shuma">数码</Link>
+                        <Link to="/caijing">财经</Link>
+                    </div>
+                    <br/>
+                    <div className={'wrapper'}>
+                        <div className={'left'}>
+                            <Route exact path="/" component={Tuijian}/>
+                            <Route path="/keji" component={Keji}/>
+                            <Route path="/redian" component={Redian}/>
+                            <Route path="/zhibo" component={Zhibo}/>
+                            <Route path="/youxi" component={Youxi}/>
+                            <Route path="/yule" component={Yule}/>
+                            <Route path="/shuma" component={Shuma}/>
+                            <Route path="/caijing" component={Caijing}/>
+                        </div>
+
+                        <div className={'right'}>
+                            <div>
+                                <Login/>
+                                <hr color={'red'}/>
+                                <Tail/>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <Me/>
+                </div>
+
+
+            </Router>
+
   );
+    }
 }
 
 export default App;

3a99dfd13c9d7109527e0395e2e7bb2117a4ca73
Peter-Hai-0 <2298288264@qq.com>
2021-02-04 22:11:47 +0800

    third commit.修改了评论页面和样式

--- src/App.js
+++ src/App.js
@@ -36,7 +36,6 @@ class App extends React.Component {
     searchMsgB = () => {
         this.searchMsgK();
     }
-
 //<Route path={'/loginPage'} component={LoginPage}></Route>
     render() {
         return (
--- src/components/comments.js
+++ src/components/comments.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from "axios";
 import like_0 from '../assets/images/like_0.png';
 import like_1 from '../assets/images/like_1.png';
+import me from '../assets/images/me.png'
 
 class Comments extends React.Component {
     constructor(props) {
@@ -14,8 +15,10 @@ class Comments extends React.Component {
         }
     }
 
+//
+    //6017fbb1c280fd0041e06074
     callAPI = () => {
-        axios.get('https://qc8vvg.fn.thelarkcloud.com/getcomment?_id=6017fbb1c280fd0041e06074')
+        axios.get('https://qc8vvg.fn.thelarkcloud.com/getcomment?_id=6018f0e4b588ae021ac5ac91')
             .then((res) => {
                 console.log(res);
                 this.setState({
@@ -45,7 +48,7 @@ class Comments extends React.Component {
             textarea_heigh: '150px'
         })
     }
-    changeLike = () => {
+    changeLike = (key) => {
         if (!this.state.likeChanged) {
             this.setState({
                 likesrc: like_1,
@@ -57,16 +60,19 @@ class Comments extends React.Component {
                 likeChanged: false
             })
         }
+        alert(key)
     }
 
     render() {
         return (
             <div className={'comment'}>
+                <meta name="referrer" content="no-referrer"/>
                 <div className={'comment-total'}>
                     <a className={'total1'}>200</a>
                     <a className={'total2'}>条评论</a>
                 </div>
                 <div className={'comment-input'} style={{height: this.state.textarea_heigh}}>
+                    <img src={me}/>
                     <textarea onMouseDown={this.resize} style={{height: this.state.textarea_heigh}}
                               placeholder="写下您的评论..."/>
                     <button type={'button'} onClick={this.commentEvent} style={{height: this.state.textarea_heigh}}>评论
@@ -77,24 +83,29 @@ class Comments extends React.Component {
                         this.state.commentlist.map((value, key) => {
                             return (
                                 <div key={key} className={'comment-writer'}>
-                                    <li>
-                                        <div className={'user-info'}>
-                                            <a style={{color: ''}}>{value.user}</a>
-                                        </div>
-                                        <div className={'comment-detail'}>
-                                            <p>{value.content}</p>
-                                        </div>
-                                        <div className={'comment-reply'}>
-                                            <a className={'comment-reply'}>回复数 {value.reply_num}</a>
-                                            <div className={'comment-like'}>
-                                                <a>{value.like}</a>
-                                                <img src={this.state.likesrc} onClick={this.changeLike}/>
+                                    <div>
+                                        <a className={'user-img'}>
+                                            <img src={value.head_img}/>
+                                        </a>
+                                    </div>
+                                    <div>
+                                        <li>
+                                            <div className={'user-info'}>
+                                                <a>{value.user}</a>
                                             </div>
-
-
-                                        </div>
-
-                                    </li>
+                                            <div className={'comment-detail'}>
+                                                <p>{value.content}</p>
+                                            </div>
+                                            <div className={'comment-reply'}>
+                                                <a className={'comment-reply'}>回复 {value.reply_num}</a>
+                                                <div className={'comment-like'}>
+                                                    <a>{value.like.length}</a>
+                                                    <img src={this.state.likesrc}
+                                                         onClick={this.changeLike.bind(this, key)}/>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </div>
                                 </div>
                             )
                         })
